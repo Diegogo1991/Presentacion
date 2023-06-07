59ddde97280f36bd1e31135615beb78267cbd408
Diego Go <sajudian85@gmail.com>
2023-06-07 16:03:36 -0500

    Funcion para validad form suscribete añadida

--- funcion.js
+++ funcion.js
@@ -52,3 +52,55 @@ function crearElemento() {
 var creador = document.getElementById('creador');
 
 creador.addEventListener('click', crearElemento);*/
+
+//VALIDACION FORMULARIOS
+
+const expresiones = {
+   usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+   password: /^.{4,12}$/, // 4 a 12 digitos.
+   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+   telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const form = document.getElementById('suscribeteform');
+const inputs = document.querySelectorAll('#suscribeteform input');
+const enviarForm = false;
+
+const validarFormulario = (e) => {
+   e.target.name;
+   switch (e.target.name) {
+      case "correo":
+         if (expresiones.correo.test(e.target.value)) {
+            console.log('funciona');
+            document.getElementById('suscribeteform').style.border = ".2rem solid white";
+            document.getElementById('correo').style.color = "aquamarine";
+            document.getElementById('enviarcorreo').style.background = "white";
+            enviarForm = true;
+         } else {
+            console.log('no funciona');
+            document.getElementById('suscribeteform').style.border = ".2rem solid rgb(248, 75, 75)";
+            document.getElementById('correo').style.color = "rgb(248, 75, 75)";
+            document.getElementById('enviarcorreo').style.background = "rgb(248, 75, 75)";
+            enviarForm = false;
+         }
+         break;
+      case "enviarcorreo":
+
+         break;
+
+      default:
+         break;
+   }
+}
+
+inputs.forEach((input) => {
+   input.addEventListener('keyup', validarFormulario);
+   input.addEventListener('blur', validarFormulario)
+})
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   if (enviarForm == true) {
+
+   }})
